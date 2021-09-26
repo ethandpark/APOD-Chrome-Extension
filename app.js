1704b95a87014e5af424997b51ef1378086993bb
Ethan Park <70242462+ethandpark@users.noreply.github.com>
2021-09-25 21:54:00 -0400

    Fix API requests
    js now properly fetches relevant data from the API and manipulates the DOM with it as desired (specifically with background image)

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const url = 'https://api.nasa.gov/planetary/apod?api_key='
-const api_key = 'WoGhgbdDufrY2DSk09YgcsrLmGOnyJReTJJG2wOl'
+const url = 'https://api.nasa.gov/planetary/apod?api_key=';
+const api_key = 'WoGhgbdDufrY2DSk09YgcsrLmGOnyJReTJJG2wOl';
 
 const getNASA = async() => {
     try {
-        const response = await fetch(url + api_key)
-        const data = await response.json()
-        console.log('NASA APOD data', data)
-        displayData(data)
+        const response = await fetch(url + api_key);
+        const data = await response.json();
+        console.log('NASA APOD data', data);
+        displayData(data);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-
 const displayData = data => {
-    document.getElementById('title').textContent = data.title
-    document.getElementById('date').textContent = data.date
-    document.getElementById('picture').src = data.hdurl
-    document.getElementById('explanation').textContent = data.explanation
+    document.body.style.cssText+=`background-image:url(${data.hdurl})`;
+    document.body.style.cssText+=`background-position: center`; 
+    document.getElementById('title').textContent = data.title;
+    document.getElementById('date').textContent = data.date;
+    document.getElementById('explanation').textContent = data.explanation;
 }
 
 getNASA();
